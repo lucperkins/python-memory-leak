c4dcb8fa550a6bef362cb28f0912caf104f144aa
Luc Perkins <luc.perkins@datadoghq.com>
2021-09-01 13:12:22 -0700

    Add rudimentary Python Lambda function

--- main.tf
+++ main.tf
@@ -12,6 +12,14 @@ terraform {
   }
 }
 
+// Providers
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "datadog" {}
+
+// Variables
 variable "aws_account_id" {
   type = string
 }
@@ -25,13 +33,70 @@ variable "datadog_aws_integration_role" {
   default = "DatadogAWSIntegrationRole"
 }
 
-provider "aws" {
-  region = "us-east-1"
+variable "lambda_dir" {
+  type    = string
+  default = "lambda"
 }
 
-provider "datadog" {}
+variable "lambda_python_runtime" {
+  type    = string
+  default = "python3.9"
+}
 
+variable "zip_file" {
+  type    = string
+  default = "leaky-cache.zip"
+}
+
+// AWS resources
+resource "aws_s3_bucket" "lambda_archives" {}
+
+data "archive_file" "memory_leak" {
+  type        = "zip"
+  source_dir  = "${path.module}/${var.lambda_dir}"
+  output_path = "${path.module}/${var.zip_file}"
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "serverless_lambda"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      }
+    ]
+  })
+}
+
+resource "aws_s3_bucket_object" "memory_leak_lambda" {
+  bucket = aws_s3_bucket.lambda_archives.id
+  key    = var.zip_file
+  source = data.archive_file.memory_leak.output_path
+  etag   = filemd5(data.archive_file.memory_leak.output_path)
+}
+
+resource "aws_lambda_function" "memory_leak" {
+  function_name    = "PythonMemoryLeak"
+  s3_bucket        = aws_s3_bucket.lambda_archives.id
+  s3_key           = aws_s3_bucket_object.memory_leak_lambda.key
+  runtime          = var.lambda_python_runtime
+  handler          = "lambda_function.lambda_handler"
+  source_code_hash = data.archive_file.memory_leak.output_base64sha256
+  role             = aws_iam_role.lambda_exec.arn
+}
+
+// Datadog resources
 resource "datadog_integration_aws" "python_memory_leak" {
   account_id = var.aws_account_id
   role_name  = var.datadog_aws_integration_role
+}
+
+resource "datadog_integration_aws_lambda_arn" "python_memory_leak_lambda" {
+  account_id = var.aws_account_id
+  lambda_arn = aws_lambda_function.memory_leak.arn
 }
