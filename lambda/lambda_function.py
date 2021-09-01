a6830f00b7d65a3d21f36f2130b82670f624cb94
Luc Perkins <luc.perkins@datadoghq.com>
2021-09-01 13:25:12 -0700

    Add basic function logic

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,2 +1,14 @@
-def lambda_handler(event, context):
-    pass
+def get_name(event: dict) -> str:
+    default_name = 'world'
+    name: str
+
+    if 'name' in event:
+        return name
+    else:
+        return default_name
+
+
+def lambda_handler(event: dict, _context) -> dict:
+    return {
+        'hello': get_name(event)
+    }
