4ec5c36a57825dd032e6e25d8a1fbff8d91cc3b9
Luc Perkins <luc.perkins@datadoghq.com>
2021-09-01 12:50:32 -0700

    Add Datadog TF provider

--- main.tf
+++ main.tf
@@ -4,9 +4,25 @@ terraform {
       source  = "hashicorp/aws"
       version = "3.56.0"
     }
+
+    datadog = {
+      source  = "DataDog/datadog"
+      version = "3.3.0"
+    }
   }
+}
+
+variable "datadog_role" {
+  type    = string
+  default = "DatadogAWSIntegrationRole"
+}
+
+variable "datadog_app_key" {
+  type = string
 }
 
 provider "aws" {
   region = "us-east-1"
 }
+
+provider "datadog" {}
