7ff9517935cb109e3654877ef1ad869d6388b2e1
Luc Perkins <luc.perkins@datadoghq.com>
2021-09-01 13:45:44 -0700

    Use blocks where needed

--- main.tf
+++ main.tf
@@ -111,12 +111,12 @@ data "aws_iam_policy_document" "datadog_aws_integration_assume_role" {
   statement {
     actions = ["sts:AssumeRole"]
 
-    principals = {
+    principals {
       type        = "AWS"
       identifiers = ["arn:aws:iam::464622532012:root"]
     }
 
-    condition = {
+    condition {
       test     = "StringEquals"
       variable = "sts:ExternalId"
       values = [
